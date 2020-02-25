3c15cf6933ef9a39b4bf93168ad1bce8007ea69d
Lubomir Lenard <40657698+lubomir1618@users.noreply.github.com>
2020-02-25 10:07:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ app.get("/admin", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
   // console.log(process.env);
-  console.log("test kudoness heroku");
+  console.log("test kudoness heroku ");
 });
