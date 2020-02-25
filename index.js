4fe3fcfd7fd5ac8605820c94d42f18f04820a533
Lubomir Lenard <lubomir.lenard@performgroup.com>
2020-02-25 07:08:08 +0100

    structure changed

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const monk = require("monk");
 const cors = require("cors");
 const app = express();
 
-app.listen(4000, () => {
-  console.log("Server is running on port 4000");
+const port = process.env.port || 4000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.json({ message: "ok" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
