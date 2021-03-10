8a857f6da4bd7aab7ccf952bdf319eaef77eaca1
Lukas Orgovan <jedi@Lukass-MacBook-Pro.local>
2021-03-10 14:30:29 +0100

    name fix

--- src/client/assets/data.tsx
+++ src/client/assets/data.tsx
@@ -1,143 +1,143 @@
 export default [
-  {
-     "name":"Andrejko Maros"
-  },
-  {
-     "name":"Angelovic Kamil"
-  },
-  {
-     "name":"Aron Martin"
-  },
-  {
-     "name":"Babela Jan"
-  },
-  {
-     "name":"Birka†Miroslav"
-  },
-  {
-     "name":"Brunacky Tomas"
-  },
-  {
-     "name":"Cernak Adrian"
-  },
-  {
-     "name":"Durisin Martin"
-  },
-  {
-     "name":"Fecko Erik"
-  },
-  {
-     "name":"Gromina Juraj"
-  },
-  {
-     "name":"Halupka Arben"
-  },
-  {
-     "name":"Halupka Ivan"
-  },
-  {
-     "name":"Hanusovsky Henrich"
-  },
-  {
-     "name":"Istvan Miroslav"
-  },
-  {
-     "name":"Jassova Monika"
-  },
-  {
-     "name":"Kazmirsky Peter"
-  },
-  {
-     "name":"Langa Adam"
-  },
-  {
-     "name":"Lencses Rastislav"
-  },
-  {
-     "name":"Lichvarova Jarmila"
-  },
-  {
-     "name":"Malatakova Miroslava"
-  },
-  {
-     "name":"Mezei Marek"
-  },
-  {
-     "name":"Mezei Stefan"
-  },
-  {
-     "name":"Miskovic Lubomir"
-  },
-  {
-     "name":"Oscipovsky Lukas"
-  },
-  {
-     "name":"Repcin Rudolf"
-  },
-  {
-     "name":"Ruda Iveta"
-  },
-  {
-     "name":"Ruscak Michal"
-  },
-  {
-     "name":"Rzuhovska Veronika"
-  },
-  {
-     "name":"Saloky Lubos"
-  },
-  {
-     "name":"Sciranka Jaroslav"
-  },
-  {
-     "name":"Sipkai Zoltan"
-  },
-  {
-     "name":"Sorad Jan"
-  },
-  {
-     "name":"Spisak Martin"
-  },
-  {
-     "name":"Sroka Jozef"
-  },
-  {
-     "name":"Svec Erik"
-  },
-  {
-     "name":"Szabova Patricia"
-  },
-  {
-     "name":"Tomko Martin"
-  },
-  {
-     "name":"Tothova Diana"
-  },
-  {
-     "name":"Ulicny Martin"
-  },
-  {
-     "name":"Vargova Veronika"
-  },
-  {
-     "name":"Vitkovic Martin"
-  },
-  {
-     "name":"Sicak Michal"
-  },
-  {
-     "name":"Alena Vancikova"
-  },
-  {
-     "name":"Samuel Neceda"
-  },
-  {
-     "name":"Bruno Kocik"
-  },
-  {
-     "name":"Andrej Kazmirsky"
-  },
-  {
-     "name":"Vojtko Robert"
-  }
-];
+    {
+       "name":"Andrejko Maros"
+    },
+    {
+       "name":"Angelovic Kamil"
+    },
+    {
+       "name":"Aron Martin"
+    },
+    {
+       "name":"Babela Jan"
+    },
+    {
+       "name":"Birka Miroslav"
+    },
+    {
+       "name":"Brunacky Tomas"
+    },
+    {
+       "name":"Cernak Adrian"
+    },
+    {
+       "name":"Durisin Martin"
+    },
+    {
+       "name":"Fecko Erik"
+    },
+    {
+       "name":"Gromina Juraj"
+    },
+    {
+       "name":"Halupka Arben"
+    },
+    {
+       "name":"Halupka Ivan"
+    },
+    {
+       "name":"Hanusovsky Henrich"
+    },
+    {
+       "name":"Istvan Miroslav"
+    },
+    {
+       "name":"Jassova Monika"
+    },
+    {
+       "name":"Kazmirsky Peter"
+    },
+    {
+       "name":"Langa Adam"
+    },
+    {
+       "name":"Lencses Rastislav"
+    },
+    {
+       "name":"Lichvarova Jarmila"
+    },
+    {
+       "name":"Malatakova Miroslava"
+    },
+    {
+       "name":"Mezei Marek"
+    },
+    {
+       "name":"Mezei Stefan"
+    },
+    {
+       "name":"Miskovic Lubomir"
+    },
+    {
+       "name":"Oscipovsky Lukas"
+    },
+    {
+       "name":"Repcin Rudolf"
+    },
+    {
+       "name":"Ruda Iveta"
+    },
+    {
+       "name":"Ruscak Michal"
+    },
+    {
+       "name":"Rzuhovska Veronika"
+    },
+    {
+       "name":"Saloky Lubos"
+    },
+    {
+       "name":"Sciranka Jaroslav"
+    },
+    {
+       "name":"Sipkai Zoltan"
+    },
+    {
+       "name":"Sorad Jan"
+    },
+    {
+       "name":"Spisak Martin"
+    },
+    {
+       "name":"Sroka Jozef"
+    },
+    {
+       "name":"Svec Erik"
+    },
+    {
+       "name":"Szabova Patricia"
+    },
+    {
+       "name":"Tomko Martin"
+    },
+    {
+       "name":"Tothova Diana"
+    },
+    {
+       "name":"Ulicny Martin"
+    },
+    {
+       "name":"Vargova Veronika"
+    },
+    {
+       "name":"Vitkovic Martin"
+    },
+    {
+       "name":"Sicak Michal"
+    },
+    {
+       "name":"Vancikova Alena"
+    },
+    {
+       "name":"Neceda Samuel"
+    },
+    {
+       "name":"Kocik Bruno"
+    },
+    {
+       "name":"Kazmirsky Andrej"
+    },
+    {
+       "name":"Vojtko Robert"
+    }
+ ];
